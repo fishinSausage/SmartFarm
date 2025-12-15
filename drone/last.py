750489bf31ef5097179b82818fafb192452d6c19
donggyu Lee <classdragon333@gmail.com>
2025-12-16 00:52:08 +0900

    drone

--- drone/last.py
+++ drone/last.py
@@ -232,6 +232,7 @@ def main(socket_client=None):
         time.sleep(.5)
         import os
 
+        # Take a picture
         os.system(
             "v4l2-ctl --device=/dev/video0 "
             "--set-fmt-video=width=1920,height=1080,pixelformat=MJPG "
